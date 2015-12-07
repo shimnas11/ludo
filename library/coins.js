6ea418a986215bf17304f85815ef9477bf9ce75f
nimeshp <nimeshp@INnimeshp.local>
2015-12-07 18:00:53 +0530

    [Vijay/Nimesh]
    What changes have you made?
    
    *	Added one more dependet functionality for killing player coin.
    
    Why do we need this change?
    
    *	It will keep the both player coin in the safe position while
    trying to kill.
    
    What can this change impact?
    
    *

--- library/coins.js
+++ library/coins.js
@@ -1,35 +1,33 @@
+var safePositions = require('./moves.js').safePositions;
 var players = {player1:{},player2:{}}
 exports.move = function(data,moves,diceValue){
-
 	var cellRoutes = moves[data.coinClass];
-		if(Object.keys(players[data.coinClass])==0 || players[data.coinClass][data.coinId]==undefined)
-			var pos = cellRoutes.indexOf(data.position);			
-		else
-			var pos = cellRoutes.indexOf(players[data.coinClass][data.coinId].position);
-
+	if(Object.keys(players[data.coinClass]) == 0 || players[data.coinClass][data.coinId]==undefined)
+		var pos = cellRoutes.indexOf(data.position);			
+	else
+		var pos = cellRoutes.indexOf(players[data.coinClass][data.coinId].position);
 	
-		data.position = cellRoutes[pos+diceValue];
-		exports.kill(data);
-		players[data.coinClass][data.coinId]=data;
-
+	data.position = cellRoutes[pos+diceValue];
+	exports.kill(data);
+	players[data.coinClass][data.coinId]=data;
 };
 
 
-exports.kill=function(data){
-	var killingPlayer1=[],killingPlayer2=[];
-	if(data.coinClass=='player1'){
-		killingPlayer2=Object.keys(players.player2).filter(function(keys){
-			return players.player2[keys].position==data.position
+exports.kill = function(data){
+	var killingPlayer1 = [],killingPlayer2 = [];
+	if(data.coinClass == 'player1'){
+		killingPlayer2 = Object.keys(players.player2).filter(function(keys){
+			return players.player2[keys].position == data.position
 		});
 	}
-	if(data.coinClass=='player2'){
-		killingPlayer1=Object.keys(players.player1).filter(function(keys){
-			return players.player1[keys].position==data.position
+	if(data.coinClass == 'player2'){
+		killingPlayer1 = Object.keys(players.player1).filter(function(keys){
+			return players.player1[keys].position == data.position
 		});
 	}
-	if(killingPlayer2.length!=0)
+	if(killingPlayer2.length != 0 && safePositions.indexOf(data.position)<0)
 		players.player2[killingPlayer2[0]].position=01;
-	if(killingPlayer1.length!=0)
+	if(killingPlayer1.length!=0 && safePositions.indexOf(data.position)<0)
 		players.player1[killingPlayer1[0]].position=00;
 };
 
--- test/coinTests.js
+++ test/coinTests.js
@@ -1,68 +1,88 @@
 var lib = require('../library/coins.js');
 var chai = require('chai');
 var assert = chai.assert;
+var paths = require('../library/moves.js').players;
 
-var path = {player1:[13,12,11,21,22,31,41,51,52,53,54,55,45,35,25,15,14,24,34,44,43],
-	player2:[53,54,4,5,32,1,2,34,2,31,11,23,44,23,24,25,43,41]};
 describe('movement of coin',function(){
 	it("player1's coin 1 should move according to the dice value",function(){
 		var data = {coinClass:'player1', coinId:1, position:13};
 		var diceValue = 2;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,11);
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player1['1'].position,11);
 	});
 	it("player1's coin 2 should move according to the dice value",function(){
 		var data = {coinClass:'player1', coinId:2, position:13};
 		var diceValue = 5;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,31);
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player1['2'].position,41);
 	});
 	it("player2's coin 1 should move according to the dice value",function(){
-		var data = {coinClass:'player2', coinId:1, position:54};
+		var data = {coinClass:'player2', coinId:1, position:53};
 		var diceValue = 4;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,1);
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player2['1'].position,35);
 	});
 	it("player2's coin 4 should move according to the dice value",function(){
-		var data = {coinClass:'player2', coinId:4, position:54};
+		var data = {coinClass:'player2', coinId:4, position:53};
 		var diceValue = 1;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,4);
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player2['4'].position,54);
 	});
 	it("player1's coin 2 should move according to the dice value from its previous position",function(){
 		var data = {coinClass:'player1', coinId:2, position:13};
-		var diceValue = 5;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,54);
+		var diceValue = 3;
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player1['2'].position,53);
 	});
 	it("player2's coin 1 should move according to the dice value from its previous position",function(){
 		var data = {coinClass:'player2', coinId:1, position:13};
 		var diceValue = 5;
-		lib.move(data,path,diceValue);
-		assert.equal(data.position,11);
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player2['1'].position,12);
 	});
 });
 
 describe("killing player1's coins by player2",function(){
-	var data = {coinClass:'player2', coinId:1, position:54};
+	var data = {coinClass:'player2', coinId:1, position:53};
 	var diceValue = 1;
 	it("once a player2 coin overlapped with player1's coin, player1's coin should go to its satarting position",function(){
-		lib.move(data,path,diceValue)
+		lib.move(data,paths,diceValue);
 		assert.equal(lib.players.player1['1'].position,0);
 	});
 	it("player2's coin should be in the position of player1",function(){
-		assert.equal(lib.players.player2['1'].position,23)
+		assert.equal(lib.players.player2['1'].position,11)
 	});
 });
 
 describe("killing player2's coins by player1",function(){
-	var data = {coinClass:'player1', coinId:1, position:54};
-	var diceValue = 1;
-	it("once a player1 coin overlapped with player2's coin, player2's coin should go to its satarting position",function(){
-		lib.move(data,path,diceValue)
-		assert.equal(lib.players.player2['1'].position,23);
+	var data = {coinClass:'player1', coinId:4, position:13};
+	var diceValue = 9;
+	it("once a player1 coin overlapped with player2's coin, player2's coin should go to its starting position",function(){
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player2['4'].position,01);
 	});
 	it("player1's coin should be in the position of player2",function(){
-		assert.equal(lib.players.player1['1'].position,13)
+		assert.equal(lib.players.player1['4'].position,54)
+	});
+});
+
+describe("safe position",function(){
+	it("a safe position can hold different players coins",function(){
+		var data = {coinClass:'player1', coinId:4, position:13};
+		var diceValue = 3;
+		lib.move(data,paths,diceValue);
+		data = {coinClass:'player2', coinId:3, position:53};
+		diceValue = 4;
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player1['4'].position,35);
+		assert.equal(lib.players.player2['3'].position,35);
+	});
+	it("a safe position can hold same player coins",function(){
+		var data = { coinClass: 'player1', coinId: 2, position: 13 };
+		var diceValue = 4;
+		lib.move(data,paths,diceValue);
+		assert.equal(lib.players.player1['2'].position,35);
+		assert.equal(lib.players.player1['4'].position,35);
+		assert.equal(lib.players.player2['3'].position,35);
 	});
 });
