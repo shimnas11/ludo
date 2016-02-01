e90efebf207fd254694139891010b95b29c02289
NavyaSri <basava@thoughtworks.com>
2016-02-01 17:55:18 +0530

    [team]- fixed the failing test in playersTest.

--- test/playerTest.js
+++ test/playerTest.js
@@ -9,13 +9,13 @@ describe("move",function(){
   	var coin = new Coin(1,'green');
   	var coins = [];
     coins[1]=coin;
-    var tile1={};
+    //var tile1={};
     var tile2={placeCoin:function(){}};
     var spy=sinon.spy(tile2,"placeCoin");
-    var path=[tile1,tile2];
+    var path=[tile2];
     var player=new Player("Sarath","green",coins,path);
     var coinId = ld.findIndex(coins,{_id:1});
-    player.move(coinId,1);
+    player.move(coinId,2);
     assert.ok(spy.withArgs(coin).calledOnce);
   });
 });
